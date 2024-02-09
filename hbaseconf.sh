cc2a5995e846fe72207060ff2fe01e49aba19cc8
Saravanan Dayalan <dayalsaravanan@gmail.com>
2024-02-09 19:33:10 +0530

    script to install and configure hbase in hadoop

--- hbaseconf.sh
+++ hbaseconf.sh
@@ -11,23 +11,22 @@ mnode='172.17.0.2'
 enode='172.17.0.5'
 nodes='172.17.0.3 172.17.0.4'
 
-for ip in $mnode $enode $nodes
-do 
-	ssh $user@$ip << EOF
+for ip in $mnode $enode $nodes; do
+	ssh $user@"$ip" <<'EOF'
 
 	if [ $ip == $mnode ]
 	then
 		wget -c http://apachemirror.wuchna.com/hbase/2.2.5/hbase-2.2.5-bin.tar.gz -P /home/$user/Downloads/
 		tar -xzf /home/$user/Downloads/hbase-2.2.5-bin.tar.gz
 		mv /home/$user/hbase-2.2.5/ /usr/local/hadoop/hbase/
-	
+
 		# copy hbase to nodes
 		scp -r /usr/local/hadoop/hbase/ $user@172.17.0.3:/usr/local/hadoop/hbase/
 		scp -r /usr/local/hadoop/hbase/ $user@172.17.0.4:/usr/local/hadoop/hbase/
-	
+
 		# hbase-site.xml
 		sed -i '/<configuration>/,/<\/configuration>/{//!d}' /usr/local/hadoop/hbase/conf/hbase-site.xml
-	
+
 		sed -i 's/<configuration>/& \
 		\n\t<property> \
 		\n\t\t<name>hbase.master<\/name> \
@@ -69,13 +68,13 @@ do
 		# region servers
 		sed -i '/localhost/s/^/#/g' /usr/local/hadoop/hbase/conf/regionservers
 		sed -i '$ a 172.17.0.3 \
-		\n172.17.0.4' /usr/local/hadoop/hbase/conf/regionservers		
+		\n172.17.0.4' /usr/local/hadoop/hbase/conf/regionservers
 	fi
-		
+
 	if [ $ip != $mnode ] && [ $ip != $enode ]
 	then
 		sed -i '/<configuration>/,/<\/configuration>/{//!d}' /usr/local/hadoop/hbase/conf/hbase-site.xml
-		
+
 		sed -i 's/<configuration>/& \
 		\n\t<property> \
 		\n\t\t<name>hbase.rootdir<\/name> \
@@ -87,14 +86,14 @@ do
 		\n\t<\/property> \
 		/' /usr/local/hadoop/hbase/conf/hbase-site.xml
 	fi
-	
+
 	# hbase-env.sh
 	sed -i 's/JAVA_HOME/& \
 	export JAVA_HOME=\/usr\/lib\/jvm\/adoptopenjdk-8-hotspot-amd64 \
-	/' /usr/local/hadoop/hbase/conf/hbase-env.sh 
+	/' /usr/local/hadoop/hbase/conf/hbase-env.sh
 	#sed -i '/# export JAVA_HOME/d' /usr/local/hadoop/hbase/conf/hbase-env.sh
 	sed -i '/HBASE_MANAGES_ZK=true/s/^#//g' /usr/local/hadoop/hbase/conf/hbase-env.sh
-	
+
 	# .bashrc
 	sed -i '$ a export HBASE_HOME=\/usr\/local\/hadoop\/hbase \
 	\nexport PATH=\$HBASE_HOME\/bin:\$PATH' /home/$user/.bashrc
